94bfdec52aeb9d88161b131f85f47a69b37fdabf
Oliver Milke <o.milke@symeda.de>
2015-12-16 13:06:13 +0100

    fixing issues from the talk

--- src/main/java/de/symeda/demo/java8/misc/MapManipulation.java
+++ src/main/java/de/symeda/demo/java8/misc/MapManipulation.java
@@ -8,14 +8,14 @@ public class MapManipulation {
 
 	private final Map<String, BigDecimal> map = new HashMap<>();
 
-	public void manipulate(String value) {
+	public void increment(String value) {
 
 		map.computeIfPresent(value, (t, u) -> u.add(BigDecimal.ONE));
 
 		map.putIfAbsent(value, BigDecimal.ONE);
 	}
 
-	public BigDecimal getValue(String value) {
+	public BigDecimal getCount(String value) {
 
 		return map.computeIfAbsent(value, v -> new BigDecimal(666L));
 	}
--- src/test/java/de/symeda/demo/java8/misc/MapManipulationTest.java
+++ src/test/java/de/symeda/demo/java8/misc/MapManipulationTest.java
@@ -11,21 +11,21 @@ import org.junit.Test;
 public class MapManipulationTest {
 
 	@Test
-	public void testGetValue() throws Exception {
+	public void testGetCount() throws Exception {
 
 		MapManipulation cut = new MapManipulation();
 
 		//value not present -> get default value
-		assertThat(cut.getValue("bad value"), is(comparesEqualTo(new BigDecimal(666L))));
+		assertThat(cut.getCount("bad value"), is(comparesEqualTo(new BigDecimal(666L))));
 
 		//but that did not insert the default, so we shall get the default again
-		assertThat(cut.getValue("bad value"), is(comparesEqualTo(new BigDecimal(666L))));
+		assertThat(cut.getCount("bad value"), is(comparesEqualTo(new BigDecimal(666L))));
 
-		cut.manipulate("asd");
-		assertThat(cut.getValue("asd"), is(comparesEqualTo(BigDecimal.ONE)));
+		cut.increment("asd");
+		assertThat(cut.getCount("asd"), is(comparesEqualTo(BigDecimal.ONE)));
 
-		cut.manipulate("asd");
-		assertThat(cut.getValue("asd"), is(comparesEqualTo(new BigDecimal(2L))));
+		cut.increment("asd");
+		assertThat(cut.getCount("asd"), is(comparesEqualTo(new BigDecimal(2L))));
 	}
 
 }
